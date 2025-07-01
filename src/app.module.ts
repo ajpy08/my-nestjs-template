df7dd6d74ae2925b8ef394cfd144cb405fb7c25a
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2025-06-30 18:41:11 -0600

    feat: add throttling support with @nestjs/throttler
    Updated package dependencies to include @nestjs/throttler for rate limiting. Modified app module to configure throttling settings and added constants for request limits.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { CommonModule } from './common/common.module';
 import { HttpExceptionFilter } from './common/filters';
+import { MAX_REQUEST_BY_MINUTE, ONE_MINUTE_TO_MS } from './common/helpers';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: ONE_MINUTE_TO_MS,
+        limit: MAX_REQUEST_BY_MINUTE,
+      },
+    ]),
     CommonModule,
   ],
   controllers: [],
--- src/common/helpers/common.constants.ts
+++ src/common/helpers/common.constants.ts
@@ -2,3 +2,6 @@ export const ZERO = 0;
 export const PROJECT_NAME = 'Template API';
 export const API_PREFIX = 'api';
 export const SWAGGER_URL = `${API_PREFIX}/v1/docs`;
+
+export const ONE_MINUTE_TO_MS = 60000;
+export const MAX_REQUEST_BY_MINUTE = 60;
