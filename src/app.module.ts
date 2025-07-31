e5600e92c084afb2598eff0f78eee93fe6a55399
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2025-07-31 17:25:18 -0600

    chore: integrate TypeORM for PostgreSQL database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,15 +2,18 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { APP_FILTER } from '@nestjs/core';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CommonModule } from './common/common.module';
 import { HttpExceptionFilter } from './common/filters';
 import { MAX_REQUEST_BY_MINUTE, ONE_MINUTE_TO_MS } from './common/helpers';
+import { postgresqlConfig } from './config/db-config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    TypeOrmModule.forRoot(postgresqlConfig),
     ThrottlerModule.forRoot([
       {
         ttl: ONE_MINUTE_TO_MS,
